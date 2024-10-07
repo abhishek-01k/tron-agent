863a05ff2e4a7d0ccb411c4e1165a30ec0fbebcb
abhishek-01k <abhishek@push.org>
2024-10-08 00:34:32 +0530

    Wallet select added

---
+++ frontend/components/WalletConnect.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+import { WalletActionButton } from '@tronweb3/tronwallet-adapter-react-ui';
+
+const WalletConnect = () => {
+    return (
+        <div>
+            <WalletActionButton />
+        </div>
+    );
+};
+
+export default WalletConnect;
